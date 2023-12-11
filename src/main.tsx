baa931f9e5540af0c60d63978272aaa39a29be91
Lukas Podmelle <podmellelukas@gmail.com>
2023-12-11 11:21:23 +0100

    1.2

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import Home from './Home.tsx';
 import './index.css';
 import { ScrollToTop } from './components/';
@@ -8,5 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<Home />
 		<ScrollToTop />
+		<Analytics />
 	</React.StrictMode>
 );
